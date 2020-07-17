a7f9e97154ffeac4841ff6f6fa318d6137d75f42
2DD4nVn8 <bp15081@shibaura-it.ac.jp>
2020-07-17 13:15:23 +0900

    update

--- public/EdIoT/js/ediot_top.js
+++ public/EdIoT/js/ediot_top.js
@@ -1,31 +1,40 @@
-window.onload=function(){
-	if(!localStorage) {
-		console.log('ローカルストレージに対応していない');
-	}
-	// セッションストレージ対応判定
-	if(!sessionStorage) {
-		console.log('セッションストレージに対応していない');
-		}
-	
-	news_uppdate();
+window.onload = function() {
+    if (!localStorage) {
+        console.log('ローカルストレージに対応していない');
+    }
+    // セッションストレージ対応判定
+    if (!sessionStorage) {
+        console.log('セッションストレージに対応していない');
+    }
+
+    news_uppdate();
 }
 
 //ローカルストレージを削除
-function removeConfig(){
-	localStorage.clear();
-	console.log("削除しました");
+function removeConfig() {
+    localStorage.clear();
+    console.log("削除しました");
 }
 
+// Newsを更新
+function news_uppdate() {
+    writeNewsData(document.getElementById('news').innerHTML);
+    Firebase.database().ref("news").once("value", function(snapshot) {
+        var logs = snapshot.val();
+        document.getElementById('news').innerHTML = logs[key].text;
 
-function news_uppdate(){
-	document.getElementById('news').innerHTML = news_get() + "\n" + document.getElementById('news').innerHTML;
+    });
+
 }
 
-function news_get(){
-	var date = new Date() ;
-	var time_unix = date.getTime() ;
-	var new_news = "update" + time_unix
-	return new_news
+function writeNewsData(text) {
+    var date = new Date();
+    var time_unix = date.getTime();
+    firebase.database().ref('news').push({
+        text: text,
+        user_id: uid,
+        date: time_unix
+    }).catch(function(error) {
+        alert(error.message);
+    });
 }
-
-
